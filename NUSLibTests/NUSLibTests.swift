25c196c5f4f0cd1a545bd8a9fbb1a57f6958ac22
kfwong <kfwong@zoho.com>
2018-03-13 19:01:22 +0800

    Initial Commit

---
+++ NUSLibTests/NUSLibTests.swift
@@ -0,0 +1,36 @@
+//
+//  NUSLibTests.swift
+//  NUSLibTests
+//
+//  Created by wongkf on 13/3/18.
+//  Copyright © 2018 nus.cs3217.nuslib. All rights reserved.
+//
+
+import XCTest
+@testable import NUSLib
+
+class NUSLibTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
