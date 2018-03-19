5dad6a6ae11a50569d82b95f2a33ee708176a03b
ramjanarthan <ram.janarthan11@gmail.com>
2018-03-19 19:32:00 +0800

    Create Book. Rename Item protocol

---
+++ NUSLib/Model/BookItem.swift
@@ -0,0 +1,34 @@
+//
+//  BookItem.swift
+//  NUSLib
+//
+//  Created by S. Ram Janarthana Raja on 17/3/18.
+//  Copyright © 2018 nus.cs3217.nuslib. All rights reserved.
+//
+
+import UIKit
+
+class BookItem: DisplayableItem {
+
+    private var title: String
+    private var thumbNail: UIImage
+    private var rating: Int
+
+    init (name title: String, image: UIImage, rating: Int) {
+        self.title = title
+        self.thumbNail = image
+        self.rating = rating
+    }
+    func getTitle() -> String {
+        return self.title
+    }
+    
+    func getThumbNail() -> UIImage {
+        return self.thumbNail
+    }
+    
+    func getRating() -> Int {
+        return self.rating
+    }
+ 
+}
