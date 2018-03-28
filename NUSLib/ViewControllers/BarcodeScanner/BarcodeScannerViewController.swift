3f2cc51c01569504fada60a87dd87ee424ca0cdd
Suyash Shekhar <suyashshekhar@gmail.com>
2018-03-28 19:21:23 +0800

    Add Barcde Scanner View Controller

---
+++ NUSLib/ViewControllers/BarcodeScanner/BarcodeScannerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BarcodeScannerViewController.swift
+//  NUSLib
+//
+//  Created by Suyash Shekhar on 28/3/18.
+//  Copyright © 2018 nus.cs3217.nuslib. All rights reserved.
+//
+
+import UIKit
+
+class BarcodeScannerViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
