62fbae4636ad0022137872044b494db0b3338fc7
Suyash Shekhar <suyashshekhar@gmail.com>
2018-04-07 01:26:17 +0800

    Add basic sidebar segue setup

--- NUSLib/ViewControllers/Sidebar/SidebarTableViewController.swift
+++ NUSLib/ViewControllers/Sidebar/SidebarTableViewController.swift
@@ -9,15 +9,13 @@
 import UIKit
 
 class SidebarTableViewController: UITableViewController {
-
+    
+    @IBOutlet var accountActionLabel: UILabel!
+    @IBOutlet var accountActionImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        setLoginCellImageAndText()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,68 +26,52 @@ class SidebarTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 4
+    }
+    
+    func setLoginCellImageAndText() {
+        // if user is signed in:
+        accountActionLabel.text = "User's name"
+        accountActionImage.image = UIImage(named: "contact")
+        
+        // else:
+        accountActionLabel.text = "Login"
+        accountActionImage.image = UIImage(named: "account")
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath == IndexPath(row: 0, section: 0) {
+            sequeToAccountOrLogin()
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    func sequeToAccountOrLogin() {
+        // if user is signed in:
+        performSegue(withIdentifier: "SidebarToAccountPage", sender: self)
+        
+        // else:
+        performSegue(withIdentifier: "SidebarToLogin", sender: self)
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+        return false
     }
-    */
 
-    /*
-    // MARK: - Navigation
+    
+    // MARK: - Navigation to the Account Page
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // Seguing to the Account Page
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.destination is AccountPageViewController {
+            // Send account details to account page?
+            // Unless that is accessible from the account page
+        }
     }
-    */
 
 }
