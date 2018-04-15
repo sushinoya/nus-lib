d9f1d4807e94e8704bbd4af7a92a83b918bd6715
Suyash Shekhar <suyashshekhar@gmail.com>
2018-04-16 01:20:20 +0800

    Add BaseModalViewController

---
+++ NUSLib/ViewControllers/BaseModalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BaseModalViewController.swift
+//  NUSLib
+//
+//  Created by Suyash Shekhar on 16/4/18.
+//  Copyright © 2018 nus.cs3217.nuslib. All rights reserved.
+//
+
+import UIKit
+
+class BaseModalViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
